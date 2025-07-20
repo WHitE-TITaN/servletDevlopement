c3a2055ae5ae4b65c80a813dc2ea39c58c0920de
WHitE-TITaN <dk488621@gmail.com>
2025-07-20 23:59:32 +0530

    ADDED SERVER LIST AND STARTUP ADDRESS

--- src/bin/headers/cppsocket.h
+++ src/bin/headers/cppsocket.h
@@ -1,1 +1,23 @@
-#include 
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iostream>
+#include <string>
+
+
+class inputSocket{
+  WSADATA winSetup;
+  SOCKET Server, Client;
+  struct sockaddr_in serverAddressList, clientAddressList;
+
+  bool startServer(){
+    int setup = WSAStartup(MAKEWORD(2, 2), &winSetup);
+    if(setup != 0){
+      std::cout << "Socket Setup Failed \n";
+      return false;
+    }
+    Server = socket(AF_INET, SOCK_STREAM, 0);
+    //if(Server == INVALID)
+  }
+  public:
+
+};
